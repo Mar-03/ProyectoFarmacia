c553dc12ccdac26606d5318ee5ef24943d613591
Jhos-hgnu <hgjofer2004@gmail.com>
2025-05-06 09:02:09 -0600

    Creando Package para la Conexión de la DB (New Package)

---
+++ src/Conector/DBConnection.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author jhosu
+ */
+public class DBConnection {
+    
+    public class ConexionBD {
+    private static final String URL = "jdbc:mysql://localhost:3306/farmacia_social?serverTimezone=UTC";
+    private static final String USUARIO = "root"; 
+    private static final String CONTRASENA = "umg$123456"; 
+
+    
+     // ESTABLECER LA CONEXIÓN
+    public static Connection getConnection() {
+    
+        Connection conn = null;
+        
+        try {
+            conn = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión de BD: " + e.getMessage());
+        }
+        
+        return conn;
+        
+    }
+}
+    
+    
+}
---
+++ src/Conector/SQL.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conector;
+
+/**
+ *
+ * @author jhosu
+ */
+public class SQL {
+    
+    //DOCUMENTO PARA HACER LAS CONSULTAS A LA BASE DE DATOS
+    
+}
