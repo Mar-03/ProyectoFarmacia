916a4dc1794d344c41300bd3098e1d8a8fb76385
Jhos-hgnu <hgjofer2004@gmail.com>
2025-05-12 13:56:29 -0600

    M(Proceso Inicio de sesion Validaciones)

--- src/Controlador/ControladorVistaInicio.java
+++ src/Controlador/ControladorVistaInicio.java
@@ -7,13 +7,14 @@ import java.awt.event.MouseListener;
 import Modelo.ModeloVistaInicio;
 import Vistas.VistaAdmin;
 import Vistas.VistaVendedor;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author jhosu
  */
-public class ControladorVistaInicio implements MouseListener{
-    
+public class ControladorVistaInicio implements MouseListener {
+
     ModeloVistaInicio modelo;
 
     public ControladorVistaInicio(ModeloVistaInicio modelo) {
@@ -21,20 +22,11 @@ public class ControladorVistaInicio implements MouseListener{
     }
 
     SesionInicioImp implementacion = new SesionInicioImp();
-    
+
     @Override
     public void mouseClicked(MouseEvent e) {
-        if(e.getComponent().equals(modelo.getVistaInicio().btnAcceder)){
-            if(tipoUsuario() == 1){
-            VistaAdmin vistaAdmin = new VistaAdmin();
-            vistaAdmin.setVisible(true);
-            modelo.getVistaInicio().dispose();    
-            } else if(tipoUsuario() == 2){
-                VistaVendedor vistaVendedor = new VistaVendedor();
-                vistaVendedor.setVisible(true);
-                modelo.getVistaInicio().dispose();
-            }
-            
+        if (e.getComponent().equals(modelo.getVistaInicio().btnAcceder)) {
+            inputIsEmpty();
         }
     }
 
@@ -48,27 +40,67 @@ public class ControladorVistaInicio implements MouseListener{
 
     @Override
     public void mouseEntered(MouseEvent e) {
-    
-        if(e.getComponent().equals(modelo.getVistaInicio().btnAcceder)){
-            modelo.getVistaInicio().btnAcceder.setBackground(new Color(50,95,110));
+
+        if (e.getComponent().equals(modelo.getVistaInicio().btnAcceder)) {
+            modelo.getVistaInicio().btnAcceder.setBackground(new Color(50, 95, 110));
         }
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        
-        if(e.getComponent().equals(modelo.getVistaInicio().btnAcceder)){
-            modelo.getVistaInicio().btnAcceder.setBackground(new Color(75,128,146));
+
+        if (e.getComponent().equals(modelo.getVistaInicio().btnAcceder)) {
+            modelo.getVistaInicio().btnAcceder.setBackground(new Color(75, 128, 146));
         }
     }
-      
-    public int tipoUsuario(){
-        
-        int usuarioIngresado = Integer.parseInt((modelo.getVistaInicio().txtUsuario.getText()));
-        
-        return usuarioIngresado;
+
+    public void vallidarUsuario(int tipoUsuario, String usuarioIngresado, String contraIngresada, String usuarioEncontrado, String contraEncontrada) {
+
+        if (tipoUsuario == 1) {
+            if (usuarioIngresado.equals(usuarioEncontrado) && contraIngresada.equals(contraEncontrada)) {
+                VistaAdmin vistaAdmin = new VistaAdmin();
+                vistaAdmin.setVisible(true);
+                modelo.getVistaInicio().dispose();
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al Iniciar Sesión, usuario o contrasenia incorrectos", "ERROR INICIO DE SESIÓN", JOptionPane.ERROR_MESSAGE);
+
+            }
+        } else if (tipoUsuario == 2) {
+            if (usuarioIngresado.equals(usuarioEncontrado) && contraIngresada.equals(contraEncontrada)) {
+                VistaVendedor vistaVendedor = new VistaVendedor();
+                vistaVendedor.setVisible(true);
+                modelo.getVistaInicio().dispose();
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al Iniciar Sesión, usuario o contrasenia incorrectos", "ERROR INICIO DE SESIÓN", JOptionPane.ERROR_MESSAGE);
+            }
+
+        }
+
     }
-    
-    
-    
+
+    public void inputIsEmpty() {
+
+        if (modelo.getVistaInicio().txtUsuario.getText().isEmpty() || modelo.getVistaInicio().txtContraseña.getText().isEmpty()) {
+            JOptionPane.showInternalMessageDialog(null, "Por favor debe de ingresar todos los datos", "ERROR \"DATOS VACIOS\"", JOptionPane.ERROR_MESSAGE);
+        } else {
+            capturaDeDatos();
+        }
+
+    }
+
+    public void capturaDeDatos() {
+
+        String usuarioIngresado = modelo.getVistaInicio().txtUsuario.getText();
+        String contraseniaIngresada = modelo.getVistaInicio().txtContraseña.getText();
+
+        ModeloVistaInicio model = implementacion.consultaUsuario(usuarioIngresado, contraseniaIngresada);
+
+        String usuarioEncontrado = modelo.getUsuarioEncontrado();
+        String contraseniaEncontrada = modelo.getContraseniaEncontrada();
+        int tipoDeUsuario = modelo.getTipoUsuario();
+
+        vallidarUsuario(tipoDeUsuario, usuarioIngresado, contraseniaIngresada, usuarioEncontrado, contraseniaEncontrada);
+
+    }
+
 }
